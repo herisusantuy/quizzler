6db014d0115ad446019ad393eef4e01f81a433cc
Heri Susanto <herisusanto.id@gmail.com>
2021-03-06 14:29:40 +0700

    Learning about MVC design pattern

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Heri Susanto on 06/03/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let q: String
+    let a: String
+}
